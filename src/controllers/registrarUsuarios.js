ed1eeb45da95951aeb9c8d25e6467767ff1b294d
Marco Calderon <marco.calderon1701@hotmail.com>
2023-12-09 15:47:46 -0500

    agregar departamento todo

--- src/controllers/registrarUsuarios.js
+++ src/controllers/registrarUsuarios.js
@@ -3,21 +3,21 @@ const bcryptjs = require("bcryptjs");
 const jwt = require("jsonwebtoken");
 
 const registrarUsuarios = async (req, res) => {
-  const {nombre_completo, dni, usuario, password, idperfil } = req.body;
+  const {nombre_completo, dni, departamento, provincia, distrito, usuario, password, idperfil } = req.body;
   const estado = 1;
 
   const passwordHasheado = await bcryptjs.hash(password, 8);
 
   try {
     const insertUsuario = `
-      INSERT INTO usuarios (nombre_completo,dni,usuario, password, estado, idperfil)
-      VALUES ($1, $2, $3, $4, $5, $6);
+      INSERT INTO usuarios (nombre_completo,dni,departamento , provincia, distrito, usuario, password, estado, idperfil)
+      VALUES ($1, $2, $3, $4, $5, $6, $7, $8 , $9);
     `;
 
     await new Promise((resolve, reject) => {
       conectar.query(
         insertUsuario,
-        [nombre_completo,dni,usuario, passwordHasheado, estado, idperfil],
+        [nombre_completo,dni,departamento, provincia, distrito, usuario, passwordHasheado, estado, idperfil],
         (error) => {
           if (error) {
             // Verificar si el error es por violación de la restricción de unicidad
